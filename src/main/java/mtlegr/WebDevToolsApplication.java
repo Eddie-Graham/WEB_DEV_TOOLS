d48e56286460b219ecb05a745e3bdd28ebe0829b
Eddie Graham <jamgrah2@aol.com>
2016-06-04 20:29:00 +0100

    Initial project commit

---
+++ src/main/java/mtlegr/WebDevToolsApplication.java
@@ -0,0 +1,12 @@
+package mtlegr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevToolsApplication.class, args);
+	}
+}
